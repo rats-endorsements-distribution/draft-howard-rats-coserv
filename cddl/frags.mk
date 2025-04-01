cd8a1fa1bfe06f24c2b44f7492127020ba9aa622
Thomas Fossati <thomas.fossati@linaro.org>
2025-04-01 11:27:16 +0200

    refactor coserv to include result set
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/frags.mk
+++ cddl/frags.mk
@@ -1,6 +1,7 @@
 COSERV_FRAGS := coserv.cddl
+COSERV_FRAGS += query.cddl
+COSERV_FRAGS += result-set.cddl
 COSERV_FRAGS += environment-selector.cddl
-COSERV_FRAGS += mini-comid.cddl
-COSERV_FRAGS += mini-cose.cddl
+COSERV_FRAGS += comid-autogen.cddl
 
 COSERV_EXAMPLES := $(wildcard examples/*.diag)
